0d2916337c056dc6540fe8ad30f327a0a0e3f740
soabel <alfredo.benaute@gmail.com>
2018-09-14 08:08:45 -0500

    actualizacion parametro controller

--- app/Http/Controllers/ParametrosController.php
+++ app/Http/Controllers/ParametrosController.php
@@ -9,13 +9,13 @@ use Carbon\Carbon;
 class ParametrosController extends Controller
 {
     public function get(){
-        $parametros=Seccion::where([['activo','=','1'],['eliminado','=','0']])->get();
+        $parametros=Parametro::where([['activo','=','1'],['eliminado','=','0']])->get();
         $data= array('status'=> true, 'data'=> $parametros);
         return $data;
     }
 
     public function getById($id){
-        $parametro = Seccion::find($id);
+        $parametro = Parametro::find($id);
       
         $data= array(
             'status'=> true, 
