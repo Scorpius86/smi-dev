68e5f64dc43ad6833e932582906bd4bd57492e62
Gustavo Galarza <gustavo.galarza@hundred.com.pe>
2019-07-08 15:53:34 -0500

    Fix de circulo para datos undefined

--- app/public/js/pages/panel/panel-conteo-marcadores.vue.js
+++ app/public/js/pages/panel/panel-conteo-marcadores.vue.js
@@ -1,8 +1,8 @@
 import GeometryLoader from '../../shared/geometry-loader.js';
 
 export default {
-  name: "vue-panel-conteo-marcadores",
-  template: `
+    name: "vue-panel-conteo-marcadores",
+    template: `
       <div v-if="display">
         <template>                            
           <div class = "panel-conteo-marcadores">
@@ -72,193 +72,193 @@ export default {
         </template>
       </div>
         `,
-  data() {
-    return {
-      title: "",
-      display: true,
-      sections: [],
-      labels: {},
-      area: 0,
-      isCircle: false,
-      geometryLoader: GeometryLoader
-    };
-  },
-  props: ["id"],
-  created() {
-
-  },
-  methods: {
-    hide: function () {
+    data() {
+        return {
+            title: "",
+            display: true,
+            sections: [],
+            labels: {},
+            area: 0,
+            isCircle: false,
+            geometryLoader: GeometryLoader
+        };
+    },
+    props: ["id"],
+    created() {
 
     },
-    DrawEventCREATED: function (e) {
-      let radius = null;
-      let isPolygon = -1;
-      const me = this;
-      let polygon = this.getCoordinates(e.layer.toGeoJSON().geometry);
-      const url = UrlAPI.base + "/secciones/intersectionPoints";
-      showLoading();
+    methods: {
+        hide: function() {
 
-      //this.geometryLoader.removeMarkerAll();
-      this.geometryLoader.addCustomLayer(e);
-      if (e.layerType === 'polygon') {
-        this.isCircle = false;
-        isPolygon = 1;
-        this.area = Math.round((L.GeometryUtil.geodesicArea(e.layer.getLatLngs()[0]) / 1000000) * 100) / 100;
-      } else if (e.layerType === 'circle') {
-        this.isCircle = true;
-        isPolygon = 0;
-        radius = e.layer.getRadius();
-        polygon = e.layer.toGeoJSON().geometry;
-        let radioKm = Number(e.layer.getRadius()) / 1000; // km
-        this.area = Math.round(radioKm * radioKm * Math.PI);
+        },
+        DrawEventCREATED: function(e) {
+            let radius = null;
+            let isPolygon = -1;
+            const me = this;
+            let polygon = this.getCoordinates(e.layer.toGeoJSON().geometry);
+            const url = UrlAPI.base + "/secciones/intersectionPoints";
+            showLoading();
 
-      }
-      let listaSecciones = [];
-      layers.forEach(obj => {
-        if (obj.id !== 'customLayer') {
-          listaSecciones.push(obj.id);
-        }
-      });
+            //this.geometryLoader.removeMarkerAll();
+            this.geometryLoader.addCustomLayer(e);
+            if (e.layerType === 'polygon') {
+                this.isCircle = false;
+                isPolygon = 1;
+                this.area = Math.round((L.GeometryUtil.geodesicArea(e.layer.getLatLngs()[0]) / 1000000) * 100) / 100;
+            } else if (e.layerType === 'circle') {
+                this.isCircle = true;
+                isPolygon = 0;
+                radius = e.layer.getRadius();
+                polygon = e.layer.toGeoJSON().geometry;
+                let radioKm = Number(e.layer.getRadius()) / 1000; // km
+                this.area = Math.round(radioKm * radioKm * Math.PI);
 
-      $.ajax({
-        url: url,
-        type: "POST",
-        dataType: "json",
-        data: { "polygon": polygon, "listaSecciones": listaSecciones, "isPolygon": isPolygon, "radius": radius },
-        success: function (res) {
-          if (typeof (res !== "undefined") && res !== null) {
-            if (res.status) {
-              if (e.layerType === 'polygon') {
-                me.buildStatistics(res.data);
-              } else if (e.layerType === 'circle') {
-                me.buildStatisticsCircle(res.tipos, res.dataCircle);
-              }
-              //me.getIntersectionPoints(res.data);
-
-              me.parentPanel.show();
             }
-          }
-        },
-        error: function (xhr, status) { },
-        complete: function (xhr, status) {
-          hideLoading();
-        }
-      });
-    },
-    getIntersectionPoints: function (data) {
-      this.geometryLoader.loadGeometrys(data);
-    },
-    buildStatistics: function (data) {
-      this.sections = [];
-      if (Array.isArray(data)) {
-        for (let i = 0; i < data.length; i++) {
-          const section = this.sections.find(item => item.id == data[i].seccion.idSeccionPadre);
-          if (!section) {
-            this.sections.push({
-              name: data[i].seccion.nombrePadre,
-              id: data[i].seccion.idSeccionPadre,
-              colorPadre: data[i].seccion.colorPadre,
-              sections: [
-                {
-                  name: data[i].seccion.nombre,
-                  id: data[i].seccion.id,
-                  count: data[i].geoJsonFile.length,
-                  color: data[i].seccion.color,
-                  parentSectionId: data[i].seccion.idSeccionPadre,
-                  sections: []
+            let listaSecciones = [];
+            layers.forEach(obj => {
+                if (obj.id !== 'customLayer') {
+                    listaSecciones.push(obj.id);
                 }
-              ]
             });
 
-          } else {
-            section.sections.push({
-              name: data[i].seccion.nombre,
-              id: data[i].seccion.id,
-              count: data[i].geoJsonFile.length,
-              color: data[i].seccion.color,
-              parentSectionId: data[i].seccion.idSeccionPadre,
-              sections: []
-            });
-          }
-        }
-      }
-    },
-    buildStatisticsCircle: function (tipos, arrayTipoData) {
-      this.sections = [];
+            $.ajax({
+                url: url,
+                type: "POST",
+                dataType: "json",
+                data: { "polygon": polygon, "listaSecciones": listaSecciones, "isPolygon": isPolygon, "radius": radius },
+                success: function(res) {
+                    if (typeof(res !== "undefined") && res !== null) {
+                        if (res.status) {
+                            if (e.layerType === 'polygon') {
+                                me.buildStatistics(res.data);
+                            } else if (e.layerType === 'circle') {
+                                me.buildStatisticsCircle(res.tipos, res.dataCircle);
+                            }
+                            //me.getIntersectionPoints(res.data);
 
-      for (var dataTmp in arrayTipoData) {
-        for (var i in arrayTipoData[dataTmp]) {
-          let tipo = tipos.find(item => item.id == dataTmp)
-          let section;
-          if(this.sections != undefined) {
-            section = this.sections.find(item => item.id == tipo.id);
-          } 
-          if (!section) {
-            this.sections.push({
-              name: tipo.descripcion,
-              id: tipo.id,
-              colorPadre: 0,
-              sections: [
-                {
-                  name: arrayTipoData[dataTmp][i].seccion,
-                  id: i,
-                  count: arrayTipoData[dataTmp][i].cantidad,
-                  color: arrayTipoData[dataTmp][i].color,
-                  parentSectionId: 0,
-                  distancia: arrayTipoData[dataTmp][i].distancia
+                            me.parentPanel.show();
+                        }
+                    }
+                },
+                error: function(xhr, status) {},
+                complete: function(xhr, status) {
+                    hideLoading();
                 }
-              ]
             });
-          } else {
-            section.sections.push({
-              name: arrayTipoData[dataTmp][i].seccion,
-              id: i,
-              count: arrayTipoData[dataTmp][i].cantidad,
-              color: arrayTipoData[dataTmp][i].color,
-              parentSectionId: 0,
-              distancia: arrayTipoData[dataTmp][i].distancia
-            });
-          }
-        }
-      }
-    },
-    getCoordinates: function (geometry) {
-      const coordinates = geometry.coordinates[0];
-      const resultCoordinates = new Array();
-      for (let i = 0; i < coordinates.length; i++) {
-        const coordinate = coordinates[i];
-        const x = coordinate[0];
-        const y = coordinate[1];
-        resultCoordinates.push(x + ' ' + y);
-      }
-      return resultCoordinates;
-    },
-    show: function () {
-      this.display = true;
-      this.title = "";
-    },
-    btnExportClick: function (e) {
-      const url = UrlAPI.base + "/export/IntersectionPoints";
-
-      showLoading();
-      $.ajax({
-        url: url,
-        type: "POST",
-        data: { "sections": this.sections, "area": this.area },
-        success: function (fileId, textStatus, request) {
-          var link = document.createElement("a");
-          document.body.appendChild(link);
-          link.download = "report.xlsx";
-          link.href = UrlAPI.base + "/export/" + fileId;
-          link.click();
-          document.body.removeChild(link);
         },
-        error: function (xhr, status) { },
-        complete: function (xhr, status) {
-          hideLoading();
+        getIntersectionPoints: function(data) {
+            this.geometryLoader.loadGeometrys(data);
+        },
+        buildStatistics: function(data) {
+            this.sections = [];
+            if (Array.isArray(data)) {
+                for (let i = 0; i < data.length; i++) {
+                    const section = this.sections.find(item => item.id == data[i].seccion.idSeccionPadre);
+                    if (section != undefined && section != null) {
+                        if (!section) {
+                            this.sections.push({
+                                name: data[i].seccion.nombrePadre,
+                                id: data[i].seccion.idSeccionPadre,
+                                colorPadre: data[i].seccion.colorPadre,
+                                sections: [{
+                                    name: data[i].seccion.nombre,
+                                    id: data[i].seccion.id,
+                                    count: data[i].geoJsonFile.length,
+                                    color: data[i].seccion.color,
+                                    parentSectionId: data[i].seccion.idSeccionPadre,
+                                    sections: []
+                                }]
+                            });
+
+                        } else {
+                            section.sections.push({
+                                name: data[i].seccion.nombre,
+                                id: data[i].seccion.id,
+                                count: data[i].geoJsonFile.length,
+                                color: data[i].seccion.color,
+                                parentSectionId: data[i].seccion.idSeccionPadre,
+                                sections: []
+                            });
+                        }
+                    }
+                }
+            }
+        },
+        buildStatisticsCircle: function(tipos, arrayTipoData) {
+            this.sections = [];
+
+            for (var dataTmp in arrayTipoData) {
+                for (var i in arrayTipoData[dataTmp]) {
+                    let tipo = tipos.find(item => item.id == dataTmp)
+                    let section;
+                    if (this.sections != undefined) {
+                        section = this.sections.find(item => item.id == tipo.id);
+                    }
+                    if (section != undefined && section != null) {
+                        if (!section) {
+                            this.sections.push({
+                                name: tipo.descripcion,
+                                id: tipo.id,
+                                colorPadre: 0,
+                                sections: [{
+                                    name: arrayTipoData[dataTmp][i].seccion,
+                                    id: i,
+                                    count: arrayTipoData[dataTmp][i].cantidad,
+                                    color: arrayTipoData[dataTmp][i].color,
+                                    parentSectionId: 0,
+                                    distancia: arrayTipoData[dataTmp][i].distancia
+                                }]
+                            });
+                        } else {
+                            section.sections.push({
+                                name: arrayTipoData[dataTmp][i].seccion,
+                                id: i,
+                                count: arrayTipoData[dataTmp][i].cantidad,
+                                color: arrayTipoData[dataTmp][i].color,
+                                parentSectionId: 0,
+                                distancia: arrayTipoData[dataTmp][i].distancia
+                            });
+                        }
+                    }
+                }
+            }
+        },
+        getCoordinates: function(geometry) {
+            const coordinates = geometry.coordinates[0];
+            const resultCoordinates = new Array();
+            for (let i = 0; i < coordinates.length; i++) {
+                const coordinate = coordinates[i];
+                const x = coordinate[0];
+                const y = coordinate[1];
+                resultCoordinates.push(x + ' ' + y);
+            }
+            return resultCoordinates;
+        },
+        show: function() {
+            this.display = true;
+            this.title = "";
+        },
+        btnExportClick: function(e) {
+            const url = UrlAPI.base + "/export/IntersectionPoints";
+
+            showLoading();
+            $.ajax({
+                url: url,
+                type: "POST",
+                data: { "sections": this.sections, "area": this.area },
+                success: function(fileId, textStatus, request) {
+                    var link = document.createElement("a");
+                    document.body.appendChild(link);
+                    link.download = "report.xlsx";
+                    link.href = UrlAPI.base + "/export/" + fileId;
+                    link.click();
+                    document.body.removeChild(link);
+                },
+                error: function(xhr, status) {},
+                complete: function(xhr, status) {
+                    hideLoading();
+                }
+            });
         }
-      });
     }
-  }
 }
